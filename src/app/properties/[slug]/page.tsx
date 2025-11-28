8099660842e405482e3a14b694fccff44ec65192
ambernegi <amber.negi@autodesk.com>
2025-11-28 19:48:35 +0530

    Update page.tsx

--- src/app/properties/[slug]/page.tsx
+++ src/app/properties/[slug]/page.tsx
@@ -1,14 +1,10 @@
-export default function PropertyPage({
-    params,
-  }: {
-    params: { slug: string };
-  }) {
-    return (
-      <div className="hero">
-        <div className="hero-content">
-          <h1>Property: {params.slug}</h1>
-          <p>Property details page coming soon.</p>
-        </div>
+export default function PropertyPage() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Property details</h1>
+        <p>Property details page coming soon.</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
